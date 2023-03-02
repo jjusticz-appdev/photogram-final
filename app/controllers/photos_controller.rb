3822ba022a666f7ead4c92914a45a636036e3b52
jjusticz <jjusticz@uchicago.edu>
2023-03-02 05:13:58 +0000

    Crusiing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,7 +14,12 @@ class PhotosController < ApplicationController
 
     @the_photo = matching_photos.at(0)
 
-    render({ :template => "photos/show.html.erb" })
+    if @current_user !=nil
+      render({ :template => "photos/show.html.erb" })
+    else
+      redirect_to("/user_sign_in", { :notice => "You have to sign in first."} )
+    end
+
   end
 
   def create
